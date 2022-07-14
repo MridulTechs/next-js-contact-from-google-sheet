1f1f1edb49de0e8d161da32588977f722e9c9579
Mridul2820 <immridulpanda2820@gmail.com>
2022-07-15 00:31:20 +0530

    center the form

--- pages/index.js
+++ pages/index.js
@@ -62,56 +62,58 @@ const ContactForm = () => {
   };
 
   return (
-    <form className="space-y-3 max-w-lg mx-auto p-5" onSubmit={submitForm}>
-      <p className="font-semibold text-2xl text-center">Contact Form</p>
-      <label className="block">
-        <span className="text-gray-700 font-semibold">Full Name</span>
-        <input
-          name="name"
-          type="text"
-          className="form-input form-field-contact"
-          placeholder="Full Name"
-          onChange={handleChange}
-        />
-      </label>
-      <label className="block">
-        <span className="text-gray-700 font-semibold">Email</span>
-        <input
-          name="email"
-          type="email"
-          className="form-input form-field-contact"
-          placeholder="Email"
-          onChange={handleChange}
-        />
-      </label>
-      <label className="block">
-        <span className="text-gray-700 font-semibold">Topic</span>
-        <input
-          name="topic"
-          type="text"
-          className="form-input form-field-contact"
-          placeholder="Topic"
-          onChange={handleChange}
-        />
-      </label>
-      <label className="block">
-        <span className="text-gray-700 font-semibold">Description</span>
-        <textarea
-          name="description"
-          className="form-textarea form-field-contact"
-          rows="3"
-          placeholder="Description"
-          onChange={handleChange}
-        />
-      </label>
+    <div className="flex justify-center items-center min-h-screen">
+      <form className="space-y-3 max-w-lg mx-auto p-5" onSubmit={submitForm}>
+        <p className="font-semibold text-2xl text-center">Contact Form</p>
+        <label className="block">
+          <span className="text-gray-700 font-semibold">Full Name</span>
+          <input
+            name="name"
+            type="text"
+            className="form-input form-field-contact"
+            placeholder="Full Name"
+            onChange={handleChange}
+          />
+        </label>
+        <label className="block">
+          <span className="text-gray-700 font-semibold">Email</span>
+          <input
+            name="email"
+            type="email"
+            className="form-input form-field-contact"
+            placeholder="Email"
+            onChange={handleChange}
+          />
+        </label>
+        <label className="block">
+          <span className="text-gray-700 font-semibold">Topic</span>
+          <input
+            name="topic"
+            type="text"
+            className="form-input form-field-contact"
+            placeholder="Topic"
+            onChange={handleChange}
+          />
+        </label>
+        <label className="block">
+          <span className="text-gray-700 font-semibold">Description</span>
+          <textarea
+            name="description"
+            className="form-textarea form-field-contact"
+            rows="3"
+            placeholder="Description"
+            onChange={handleChange}
+          />
+        </label>
 
-      <button
-        className="bg-green-200 px-3 py-1 font-semibold shadow-md rounded-md"
-        type="submit"
-      >
-        Send Message
-      </button>
-    </form>
+        <button
+          className="bg-green-200 px-3 py-1 font-semibold shadow-md rounded-md"
+          type="submit"
+        >
+          Send Message
+        </button>
+      </form>
+    </div>
   );
 };
 
